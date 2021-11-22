72dc19fac8c509d2fadd9c73229b810f8e004ce7
Fernando Santis <fpsantis@gmail.com>
2021-11-22 05:50:32 -0300

    Testes 1 - Apresentando Incidentes

--- app/src/main/java/com/santis/minharua/util/Testes.kt
+++ app/src/main/java/com/santis/minharua/util/Testes.kt
@@ -12,9 +12,24 @@ class Testes {
         fun populaTestes(contexto: Context) {
             val db = MinhaRuaDatabase.abrirBanco(contexto)
             GlobalScope.launch {
-                for (x in 1..23) {
+                for (x in 1..5) {
                     db.incidenteDao().inserirIncidente(
-                        Incidente(null, "Problema $x", "Aqui no minha rua está ocorrendo um grave problema do tipo $x que não tem mais jeito.", R.drawable.ic_city, x)
+                        Incidente(null, "Problema $x", "Aqui no minha rua está ocorrendo um grave problema do tipo $x que não tem mais jeito.", R.drawable.ic_city, "13660-040", x)
+                    )
+                }
+                for (x in 6..10) {
+                    db.incidenteDao().inserirIncidente(
+                        Incidente(null, "Problema $x", "Aqui no minha rua está ocorrendo um grave problema do tipo $x que não tem mais jeito.", R.drawable.ic_city, "13660-200", x)
+                    )
+                }
+                for (x in 11..20) {
+                    db.incidenteDao().inserirIncidente(
+                        Incidente(null, "Problema $x", "Aqui no minha rua está ocorrendo um grave problema do tipo $x que não tem mais jeito.", R.drawable.ic_city, "13660-039", x)
+                    )
+                }
+                for (x in 17..21) {
+                    db.incidenteDao().inserirIncidente(
+                        Incidente(null, "Problema $x", "Aqui no minha rua está ocorrendo um grave problema do tipo $x que não tem mais jeito.", R.drawable.ic_city, "13660-100", x)
                     )
                 }
             }
